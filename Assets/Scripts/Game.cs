804a047bbb0a389ab68d1bb6c20228067a357d2d
Billy Askefors <b_askefors@hotmail.com>
2016-03-21 19:14:33 +0100

    Master

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+    public GameObject cube;
+    public Camera mainCamera;
+
+    private Vector3 startPosition = new Vector3(0, 0, 0);
+	// Use this for initialization
+	void Start () {
+        Instantiate(cube, startPosition, Quaternion.identity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        { // if left button pressed...
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("pew pew");
+                GameObject currentObject = hit.transform.gameObject;
+                currentObject.transform.Translate(new Vector3(5, 5, 5));
+                
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Number.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Number : MonoBehaviour {
+    public int Value { get; set; }
+
+    public Number(int value)
+    {
+        Value = value;
+    }
+}
